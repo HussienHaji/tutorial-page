4b6072af97df05ecd54702f19f307c85e8e54faf
HussienHaji <hussienhaji.dev@gmail.com>
2024-02-16 22:30:14 +0330

    finishing front end

--- script.js
+++ script.js
@@ -49,3 +49,20 @@ $("#tutorial-slider").owlCarousel({
     },
   },
 });
+
+$("#post-slider").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items:3,
+    },
+  },
+});
